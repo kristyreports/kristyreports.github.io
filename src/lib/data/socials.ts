1a0568c85947888d6b7b3bf464eb769219932e1e
Mary <magicmarymoore@gmail.com>
2026-06-15 21:11:45 -0500

    Update socials

--- src/lib/data/socials.ts
+++ src/lib/data/socials.ts
@@ -8,8 +8,9 @@ const socialMedias: Social[] = [
   { href: 'https://www.instagram.com/kristyreports/', icon: 'fa-brands fa-instagram', label: 'Instagram' },
   { href: 'https://www.tiktok.com/@kristyreports/', icon: 'fa-brands fa-tiktok', label: 'TikTok' },
   { href: 'http://Kristyreports.substack.com', icon: 'fa-solid fa-book-bookmark', label: 'Substack' },
+  { href: 'https://muckrack.com/kristy-moore', icon: 'fa-solid fa-newspaper', label: 'Muck Rack' },
   { href: 'https://linkedin.com/in/kristyqmoore', icon: 'fa-brands fa-linkedin', label: 'LinkedIn' },
-  { href: 'mailto:kristyreports@gmail.com', icon: 'fa-regular fa-envelope', label: 'Email' },
+  { href: 'mailto:kristyreports@gmail.com', icon: 'fa-solid fa-envelope', label: 'Email' },
 ];
 
 export default socialMedias;
